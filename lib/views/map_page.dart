e2d874b417ac6cb36e7e4f1c0c02dc8132a82eb4
ehiggins98 <ericdhiggins@gmail.com>
2019-01-21 22:50:39 -0600

    Simple menu page

---
+++ lib/views/map_page.dart
@@ -0,0 +1,15 @@
+import 'dart:core';
+
+import 'package:flutter/material.dart';
+
+class MapPage extends StatefulWidget {
+  @override
+  _MapState createState() => new _MapState();
+}
+
+class _MapState extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold();
+  }
+}
