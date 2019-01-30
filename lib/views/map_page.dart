b45081f407cbdcbc7d7857a110ee2974d3f592bb
ehiggins98 <ericdhiggins@gmail.com>
2019-01-30 09:25:24 -0600

    Stuff

--- lib/views/map_page.dart
+++ lib/views/map_page.dart
@@ -1,6 +1,7 @@
 import 'dart:core';
 
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   @override
@@ -8,8 +9,19 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapState extends State<MapPage> {
+
+  var mapController;
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold();
+    return new Scaffold(
+      body: new GoogleMap(onMapCreated: _onMapCreated)
+    );
   }
 }
